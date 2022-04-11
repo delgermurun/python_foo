143e1fe24e0839ae08ea6b518c4b7e5cbe58b8ec
Delgermurun Purevkhuu <delgermurun@users.noreply.github.com>
2022-04-11 10:38:06 +0200

    foo bar 1

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='foo',
+      version='0.1',
+      # description='The funniest joke in the world',
+      # url='http://github.com/delgermurun/python_foo',
+      packages=['foo'],
+)
